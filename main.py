5126e0a8a0055201337fe601a03a834e2acf0c0a
calmis <calmis87@gmail.com>
2021-03-19 15:33:56 +0200

    Create all classes

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import Tk
+from minesweeper import Minesweeper
+
+window = Tk()
+window.title('Sapior')
+
+m = Minesweeper(window)
+
+window.mainloop()
---
+++ minesweeper.py
@@ -0,0 +1,44 @@
+from tkinter import Entry
+
+
+class TopWindow:
+    def __init__(self, window, nr_bombs):
+        self.nr_bombs = nr_bombs
+
+
+class UnderCellField:
+    def __init__(self, window, width, height):
+        pass
+
+
+class UnderCell:
+    def __init__(self):
+        pass
+
+
+class OverCellField:
+    def __init__(self, window, width, height):
+        pass
+
+
+class OverCell:
+    def __init__(self):
+        pass
+
+
+class FrameOfCells:
+    def __init__(self, window, width, height):
+        under_cells_field = UnderCellField(window, width, height)
+        over_cells_field = OverCellField(window, width, height)
+
+
+class Minesweeper:
+
+    def __init__(self, window, width=15, height=12):
+        self.nr_bombs = int(width * height) / 6
+        self.total_cells = width * height
+        self.width = width
+        self.height = height
+
+        self.top_window = TopWindow(window, self.nr_bombs)
+        self.frame_of_cells = FrameOfCells(window, width, height)
